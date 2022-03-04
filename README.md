1730a427c142814b6a5b93f84a778558bf6adc5e
Rick <95931094+confusedicarus@users.noreply.github.com>
2022-03-03 17:07:41 -0700

    finished configuring the JS to generate a readme file based on user input

---
+++ README.md
@@ -0,0 +1,23 @@
+# README Generator ![MIT](https://img.shields.io/badge/License-MIT%20-green)
+
+## Description
+    When a user answers the required prompts the index.js file will populate a custom README.md file.
+    
+    
+## Table of Contents
+    
+    - [Usage](#usage)
+    - [Credits](#credits)
+    - [License](#license)
+    
+## Installation
+    
+    
+## Usage
+    
+## Credits
+    @confusedicarus
+    rlmor1023@gmail.com
+    ## License
+
+    
--- index.js
+++ index.js
@@ -1,7 +1,71 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your GitHub User Name?",
+      name: "userName",
+    },
+    {
+      type: "input",
+      message: "What is your email?",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "what is your project's name?",
+      name: "projectTitle",
+    },
+    {
+      type: "input",
+      message: "Please write a short description of your project",
+      name: "description",
+    },
+    {
+      type: "list",
+      message: "what kind of license should your project have?",
+      name: "license",
+      choices: [
+        `![MIT](https://img.shields.io/badge/License-MIT%20-green)`,
+        `![mpl-2.0](https://img.shields.io/badge/License-mpl--2.0-green)`,
+        `![osl-3.0](https://img.shields.io/badge/License-osl--3.0-green)`,
+        `![ms-pl](https://img.shields.io/badge/License-ms--pl-green)`,
+        `![gpl-3.0](https://img.shields.io/badge/License-gpl--3.0-green)`,
+      ],
+    },
+  ])
+  .then((data) => {
+    const readMe = `# ${data.projectTitle} ${data.license}
+
+## Description
+    ${data.description}
+    
+    
+## Table of Contents
+    
+    - [Usage](#usage)
+    - [Credits](#credits)
+    - [License](#license)
+    
+## Installation
+    
+    
+## Usage
+    
+## Credits
+    @${data.userName}
+    ${data.email}
+    ## License
+
+    `;
+    fs.writeFile("README.md", readMe, (err) => {
+      err ? console.error(err) : console.log("README.md has been written");
+    });
+  });
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
